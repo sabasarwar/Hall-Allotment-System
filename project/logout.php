c8c70a80a9155cc7240ab01276ce48eb5c2e3ad5
tss <sabasarwar1997@gmail.com>
2020-06-03 14:48:55 +0530

    .gitignore fix

--- project/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['student']);
-
-echo "<script>alert('Logged out Successfully !!!')</script>";
-echo "<script>window.open('student.php','_self')</script>";
-
-?>
--- project/logoutadmin.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['admin']);
-echo "<script>alert('Logged out Successfully !!!')</script>";
-echo "<script>window.open('admin.php','_self')</script>";
-?>
