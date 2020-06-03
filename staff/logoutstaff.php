2beba4124459daa961437660e4986e1b87d29241
tss <sabasarwar1997@gmail.com>
2020-06-03 14:50:33 +0530

    .gitignore fix

--- staff/logoutstaff.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['staff']);
-echo "<script>alert('Logged out Successfully !!!')</script>";
-echo "<script>window.open('index.php#staffmember','_self')</script>";
-?>
--- student/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['student']);
-
-echo "<script>alert('Logged out Successfully !!!')</script>";
-echo "<script>window.open('index.php#student','_self')</script>";
-
-?>
